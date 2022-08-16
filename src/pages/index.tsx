828586bcad47c0965bc21eaa9ca12cbe74a0f0e7
Lucas Cardoso <lucascardoso@Lucass-MacBook-Pro.local>
2022-08-16 10:05:31 -0400

    feat: loading when click login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { DefaultButton } from '../components/DefaultButton'
 import { Logo } from '../components/Logo'
 import { useAuthContext } from '../components/context/AuthContext'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { supabase } from '../services/supabase'
 
 export default function Home(){
@@ -11,6 +11,12 @@ export default function Home(){
   const {user, login} = useAuthContext()
   const session = supabase.auth.session()
   const router = useRouter()
+  const [loading, setLoading] = useState(false)
+
+  function handleLogin(){
+    setLoading(true)
+    login()
+  }
 
   useEffect(() => {
       user?.email && router.push("/todos")
@@ -22,10 +28,10 @@ export default function Home(){
       <Logo />
 
       <DefaultButton
-      onClick={login}
+      onClick={handleLogin}
       >
           {
-            session?.user ?
+            session?.user || loading ?
             <Spinner color="black"/>
             :
             <Text>Sign In With Google</Text>
