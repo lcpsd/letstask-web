a10adeb55d60dd03bc6a4047db62965c5e65d7e4
Lucas Cardoso <lucascardoso@Lucass-MacBook-Pro.local>
2022-08-16 00:26:01 -0400

    update: next.config.js. Eslint disabled

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images:{
-    domains: ['lh3.googleusercontent.com']
-  }
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'lh4.googleusercontent.com',
+      'lh5.googleusercontent.com',
+      'lh6.googleusercontent.com',
+    ],
+  },
 }
 
 module.exports = nextConfig
